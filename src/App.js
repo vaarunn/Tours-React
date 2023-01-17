97493835a149987132b6a8325b60dae51cb5aa0b
vaarun18 <varunsadananda108@gmail.com>
2023-01-17 21:31:46 +0530

    basic react app with fetching data from an API feature

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import Tours from './Tours';
+import Tour from './Tour';
+import Loading from './Loading';
+const url = 'https://course-api.com/react-tours-project';
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState('');
+
+  const removeTours = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  //we need fetchData to be invoked first time app is rendered
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchData()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTours} />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Tours.js
@@ -0,0 +1,19 @@
+import Tour from './Tour';
+
+const Tours = ({ tours, removeTours }) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2>our tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTours={removeTours} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
